017a951e61145ccadce871132220390bf33d8419
artemantsitovich <artem.antsitovich@gmail.com>
2024-05-20 13:54:41 -0700

    init commit

---
+++ src/main/java/org/home/kinonight/KinoNightApplication.java
@@ -0,0 +1,13 @@
+package org.home.kinonight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KinoNightApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KinoNightApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/home/kinonight/KinoNightApplicationTests.java
@@ -0,0 +1,13 @@
+package org.home.kinonight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KinoNightApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
